89bb269b3822620c6ac2825d78abc13d8dff5af1
LuposX <schuppsimon5gmail.com>
2025-10-02 12:23:49 +0200

    added nix flake and new read.ipy

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "SpikeSznth Dev Shell";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs";
+
+  outputs = { self, nixpkgs, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        buildInputs = [
+          # Include Python with your packages
+          (pkgs.python3.withPackages (ps: with ps; [
+            pandas
+            requests
+            numpy
+            torch
+            matplotlib
+            jupyterlab
+            polars
+          ]))
+        ];
+
+        shellHook = ''
+          echo "🐍 Python dev environment loaded!"
+          python --version
+        '';
+      };
+    };
+}
