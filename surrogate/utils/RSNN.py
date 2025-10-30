0955fcbfe0c0711629ec7911ce4ce59fca667d0f
LuposX <schuppsimon5gmail.com>
2025-10-30 14:02:08 +0100

    sweep

--- surrogate/utils/RSNN.py
+++ surrogate/utils/RSNN.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
     
 import snntorch as snn
+from snntorch._layers.bntt import BatchNormTT1d
+
 import pytorch_lightning as L
 import torch
 import torch.nn as nn
@@ -14,11 +16,13 @@ class SpikeSynth(L.LightningModule):
                  optimizer_class,
                  lr,
                  batch_size,
-                 gamma,
+                 dropout,
                  surrogate_gradient,
                  max_epochs,
                  temporal_skip,
                  layer_skip,
+                 use_bntt,
+                 bntt_time_steps=None,
                  train_dataset=None,
                  valid_dataset=None,
                  ):
@@ -46,8 +50,8 @@ class SpikeSynth(L.LightningModule):
             batch_size (int): 
                 Batch size used for both training and validation DataLoaders.
 
-            gamma (float): 
-                Decay factor for the learning rate scheduler (if used with ExponentialLR).
+            dropout (float):
+                Value between [0, 1] if non zero, introduces a dropout layer.
 
             surrogate_gradient (torch.autograd.Function): 
                 Surrogate gradient function for backpropagation through spikes
@@ -65,6 +69,13 @@ class SpikeSynth(L.LightningModule):
                 Number of layers to skip for inter-layer residual connections.
                 For example, `layer_skip=1` adds a connection from each layer
                 to the one immediately above it. If `0`, no layer skip is applied.
+
+            use_bntt (bool):
+                Whether to use 1D Batch Normalization Layer with length time_steps. 
+                Uses Batch Normalisation Through Time (BNTT).
+
+            bntt_time_steps (None or int):
+                Batch Normalization Layer requries the amount of time steps that will be used i.e. sequecne length.
 
             train_dataset (torch.utils.data.Dataset, optional): 
                 Dataset used for training. Required by `train_dataloader()`.
@@ -86,14 +97,14 @@ class SpikeSynth(L.LightningModule):
 
         self.norm = nn.LayerNorm(self.num_inputs + self.num_params)
         self.lif_layers = nn.ModuleList()
-        self.residual_alphas = nn.ParameterList(
-            [nn.Parameter(torch.ones(1)) for _ in range(self.hparams.num_hidden_layers)]
-        )
-        self.residual_projs = nn.ModuleList()
+
+        self.temp_skip_projs = nn.ModuleList()
         
         # For layer-wise skip connections
-        self.layer_skip_alphas = nn.ParameterList()
         self.layer_skip_projs = nn.ModuleList()
+
+        # Initialize BNTT modules if requested
+        self.layer_bntt = nn.ModuleList()
         
         input_size = self.num_inputs + self.num_params
         for i in range(self.hparams.num_hidden_layers):
@@ -102,29 +113,40 @@ class SpikeSynth(L.LightningModule):
                     input_size=input_size,
                     hidden_size=self.hparams.num_hidden,
                     beta=self.hparams.beta,
-                    spike_grad=self.surrogate_gradient
+                    spike_grad=self.surrogate_gradient,
+                    dropout=self.hparams.dropout
                 )
             )
             
             # Temporal residual projection
             if input_size != self.hparams.num_hidden and self.hparams.temporal_skip != -1:
-                self.residual_projs.append(nn.Linear(input_size, self.hparams.num_hidden))
+                self.temp_skip_projs.append(nn.Linear(input_size, self.hparams.num_hidden))
             else:
-                self.residual_projs.append(nn.Identity())
+                self.temp_skip_projs.append(nn.Identity())
             
             # Layer-wise skip projection
             if self.hparams.layer_skip > 0 and i >= self.hparams.layer_skip:
-                self.layer_skip_alphas.append(nn.Parameter(torch.ones(1)))
                 self.layer_skip_projs.append(nn.Linear(self.hparams.num_hidden, self.hparams.num_hidden))
             else:
-                self.layer_skip_alphas.append(None)
                 self.layer_skip_projs.append(None)
+
+            if self.hparams.use_bntt:
+                if self.hparams.bntt_time_steps is None:
+                    raise ValueError("bntt_time_steps must be specified if use_bntt=True")
+                else:
+                    self.layer_bntt.append(BatchNormTT1d(self.hparams.num_hidden, self.hparams.bntt_time_steps))  
+            else:
+                self.layer_bntt.append(None)
+                
             
             input_size = self.hparams.num_hidden
 
         self.output_layer = nn.Linear(self.hparams.num_hidden, self.num_outputs)
          
     def forward(self, x, params=None):
+        # To log average amount of spikes
+        self.spike_counts = []
+        
         # Ensure x is 3D: (batch, seq_len, 1)
         if x.dim() == 2:
             x = x.unsqueeze(-1)
@@ -163,20 +185,29 @@ class SpikeSynth(L.LightningModule):
         for i, lif in enumerate(self.lif_layers):
             lif_out = lif(x_seq)
 
+            self.spike_counts.append(lif_out.mean())
+
+            # Apply Batch Normalisation Through Time (BNTT)
+            if self.hparams.use_bntt:
+                lif_out_norm = []
+                for t in range(lif_out.shape[0]):  # T dimension
+                    lif_out_norm.append(self.layer_bntt[i][t](lif_out[t]))
+                lif_out = torch.stack(lif_out_norm, dim=0)
+
             # Temporal skip connection
             if skip_k == -1 or skip_k >= T:
                 x_seq = lif_out
             else:
                 prev = torch.zeros_like(x_seq)
                 prev[skip_k:] = x_seq[:-skip_k]
-                prev_proj = self.residual_projs[i](prev)
-                x_seq = lif_out + self.residual_alphas[i] * prev_proj
+                prev_proj = self.temp_skip_projs[i](prev)
+                x_seq = lif_out + prev_proj
 
             # Layer-wise skip connection
             if self.hparams.layer_skip > 0 and i >= self.hparams.layer_skip:
                 skip_layer_out = layer_outputs[i - self.hparams.layer_skip]
                 skip_proj = self.layer_skip_projs[i](skip_layer_out)
-                x_seq = x_seq + self.layer_skip_alphas[i] * skip_proj
+                x_seq = x_seq + skip_proj
 
             layer_outputs.append(x_seq)
 
@@ -191,6 +222,13 @@ class SpikeSynth(L.LightningModule):
         outputs = self(X_batch)
         loss = torch.nn.MSELoss()(outputs, y_batch.float())
         self.log("train_loss", loss, prog_bar=True, on_step=False, on_epoch=True)
+
+        spike_logs = {
+            f"spikes/train_avg_layer_{i}": avg_spikes
+            for i, avg_spikes in enumerate(self.spike_counts)
+        }
+        self.log_dict(spike_logs, on_step=False, on_epoch=True, sync_dist=True)
+        
         return loss
 
     def validation_step(self, batch, batch_idx):
